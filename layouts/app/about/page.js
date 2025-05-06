191364a279cb9062d8b91144e8651a3457863792
Saadkhan0105 <saadkhan9560@gmail.com>
2025-05-06 16:59:18 +0530

    Layouts

---
+++ layouts/app/about/page.js
@@ -0,0 +1,20 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      
+      <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="text-lg text-gray-700">
+        Facebook is a social utility that connects people with friends and
+        others who work, study and live around them.
+      </p>
+      
+    </div>
+    
+  );
+};
+
+export default page;
